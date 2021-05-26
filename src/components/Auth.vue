ae751ee3d49c2d4adf3052129d01e47d2eb0f4bc
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-05-26 15:09:34 +0100

    Validation Components

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -95,12 +95,13 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form v-show="tab === 'register'">
+          <vee-form v-show="tab === 'register'">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input
+              <vee-field
                 type="text"
+                name="name"
                 class="
                   block w-full py-1.5 px-3 text-gray-800 border border-gray-300
                   transition duration-500 focus:outline-none focus:border-black rounded
@@ -186,7 +187,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
         </div>
       </div>
     </div>
