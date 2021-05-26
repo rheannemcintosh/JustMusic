66ae2a48b598bf472d7f58e476eb32be0cbbad69
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-05-26 23:16:21 +0100

    Show alert message

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -95,6 +95,13 @@
             </button>
           </form>
           <!-- Registration Form -->
+          <div
+            class="text-white text-center font-bold p-5 mb-4"
+            v-if="reg_show_alert"
+            :class="reg_alert_variant"
+          >
+            {{ reg_alert_msg }}
+          </div>
           <vee-form
             v-show="tab === 'register'"
             :validation-schema="schema"
@@ -209,6 +216,7 @@
             </div>
             <button
               type="submit"
+              :disabled="reg_in_submission"
               class="
                 block w-full bg-purple-600 text-white py-1.5 px-3
                 rounded transition hover:bg-purple-700
@@ -243,6 +251,10 @@ export default {
       userData: {
         country: 'USA',
       },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! Your account is being created.',
     };
   },
   computed: {
@@ -251,6 +263,13 @@ export default {
   methods: {
     ...mapMutations(['toggleAuthModal']),
     register(values) {
+      this.reg_show_alert = true;
+      this.reg_in_submission = true;
+      this.reg_alert_variant = 'bg-blue-500';
+      this.reg_alert_msg = 'Please wait! Your account is being created.';
+
+      this.reg_alert_variant = 'bg-green-500';
+      this.reg_alert_msg = 'Success! Your account has been created.';
       console.log(values);
     },
   },
