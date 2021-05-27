5d81c0b14581c3f2d77d2c4215db170ed5c64254
rheannemcintosh <rheannemcintosh@gmail.com>
2021-05-27 02:42:47 +0100

    Validate the login form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -59,30 +59,38 @@
           </ul>
 
           <!-- Login Form -->
-          <form v-show="tab === 'login'">
+          <vee-form
+            :validation-schema="loginSchema"
+            v-show="tab === 'login'"
+            @submit="login"
+          >
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
                 type="email"
+                name="email"
                 class="
                 block w-full py-1.5 px-3 text-gray-800 border border-gray-300
                 transition duration-500 focus:outline-none focus:border-black rounded
                 "
                 placeholder="Enter Email"
               />
+              <ErrorMessage class="text-red-600" name="email" />
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="password"
                 class="
                   block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded
                 "
                 placeholder="Password"
               />
+              <ErrorMessage class="text-red-600" name="password" />
             </div>
             <button
               type="submit"
@@ -93,7 +101,7 @@
             >
               Submit
             </button>
-          </form>
+          </vee-form>
           <!-- Registration Form -->
           <div
             class="text-white text-center font-bold p-5 mb-4"
@@ -255,6 +263,10 @@ export default {
       reg_show_alert: false,
       reg_alert_variant: 'bg-blue-500',
       reg_alert_msg: 'Please wait! Your account is being created.',
+      loginSchema: {
+        email: 'required|email',
+        password: 'required|min:3|max:100',
+      },
     };
   },
   computed: {
@@ -270,6 +282,9 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
+      console.log(values);
+    },
+    login(values) {
       console.log(values);
     },
   },
