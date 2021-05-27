b4e35a09c7fb2d757628476b021440e552b67b1a
rheannemcintosh <rheannemcintosh@gmail.com>
2021-05-27 03:05:39 +0100

    Validate the login form with an alert

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,12 @@
 <template>
   <!-- Login Form -->
+  <div
+    class="text-white text-center font-bold p-4 mb-4"
+    v-if="login_show_alert"
+    :class="login_alert_variant"
+  >
+    {{ login_alert_msg }}
+  </div>
   <vee-form
     :validation-schema="loginSchema"
     @submit="login"
@@ -34,6 +41,7 @@
   </div>
   <button
       type="submit"
+      :disabled="login_in_submission"
       class="
       block w-full bg-purple-600 text-white py-1.5 px-3
       rounded transition hover:bg-purple-700
@@ -53,10 +61,21 @@ export default {
         email: 'required|email',
         password: 'required|min:3|max:100',
       },
+      login_in_submission: false,
+      login_show_alert: false,
+      login_alert_variant: 'bg-blue-500',
+      login_alert_msg: 'Please wait! We are logging you in.',
     };
   },
   methods: {
     login(values) {
+      this.login_show_alert = true;
+      this.login_in_submission = true;
+      this.login_alert_variant = 'bg-blue-500';
+      this.login_alert_msg = 'Please wait! We are logging you in.';
+
+      this.login_alert_variant = 'bg-green-500';
+      this.login_alert_msg = 'Success! Your are now logged in.';
       console.log(values);
     },
   },
